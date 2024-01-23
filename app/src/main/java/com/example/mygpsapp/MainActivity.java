f640d157746bfb7502fb6637ac117b483dda10c5
HenrikVP <hpo@tec.dk>
2024-01-23 09:50:07 +0100

    Break

--- app/src/main/java/com/example/mygpsapp/MainActivity.java
+++ app/src/main/java/com/example/mygpsapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.mygpsapp;
 
+import android.Manifest;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +16,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        permissions();
     }
 
+    private void permissions() {
+        ActivityResultLauncher<String[]> locationPermissionRequest = registerForActivityResult(
+                new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+            Boolean fineLocationGranted =
+                    result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
+            Boolean coarseLocationGranted =
+                    result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false);
+            if (fineLocationGranted != null && fineLocationGranted) {
+                // Precise location access granted.
+            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                // Only approximate location access granted.
+            } else {
+                // No location access granted.
+                Toast.makeText(this, "F... U. Det er altså en GPS app, dummy.",
+                        Toast.LENGTH_LONG).show();
+                this.finish();
+            }
+        });
 
+// ...
 
+// Before you perform the actual permission request, check whether your app
+// already has the permissions, and whether your app needs to show a permission
+// rationale dialog. For more details, see Request permissions.
+        locationPermissionRequest.launch(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION});
+    }
 }
