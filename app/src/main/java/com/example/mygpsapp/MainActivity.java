a790b4c94863ad2303dc2667bb9f3b7208ef6e0b
HenrikVP <hpo@tec.dk>
2024-01-23 10:49:06 +0100

    Works

--- app/src/main/java/com/example/mygpsapp/MainActivity.java
+++ app/src/main/java/com/example/mygpsapp/MainActivity.java
@@ -5,39 +5,50 @@ import android.Manifest;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient flpc;
+    private TextView txt_Lat, txt_Lon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        permissions();
+        flpc = LocationServices.getFusedLocationProviderClient(this);
+        txt_Lat = findViewById(R.id.tv_latitude);
+        txt_Lon = findViewById(R.id.tv_longitude);
+        getUpdates();
     }
 
     private void permissions() {
         ActivityResultLauncher<String[]> locationPermissionRequest = registerForActivityResult(
                 new ActivityResultContracts.RequestMultiplePermissions(), result -> {
-            Boolean fineLocationGranted =
-                    result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
-            Boolean coarseLocationGranted =
-                    result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false);
-            if (fineLocationGranted != null && fineLocationGranted) {
-                // Precise location access granted.
-            } else if (coarseLocationGranted != null && coarseLocationGranted) {
-                // Only approximate location access granted.
-            } else {
-                // No location access granted.
-                Toast.makeText(this, "F... U. Det er altså en GPS app, dummy.",
-                        Toast.LENGTH_LONG).show();
-                this.finish();
-            }
-        });
+                    Boolean fineLocationGranted =
+                            result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
+                    Boolean coarseLocationGranted =
+                            result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                    if (fineLocationGranted != null && fineLocationGranted) {
+                        // Precise location access granted.
+                        getUpdates();
+                    } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                        // Only approximate location access granted.
+                    } else {
+                        // No location access granted.
+                        Toast.makeText(this, "F... U. Det er altså en GPS app, dummy.",
+                                Toast.LENGTH_LONG).show();
+                        this.finish();
+                    }
+                });
 
 // ...
 
@@ -47,5 +58,22 @@ public class MainActivity extends AppCompatActivity {
         locationPermissionRequest.launch(new String[]{
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION});
+    }
+
+    private void getUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            permissions();
+            return;
+        }
+
+        flpc.getLastLocation().addOnSuccessListener(this, location -> {
+            if (location != null) {
+                txt_Lat.setText(String.valueOf(location.getLatitude()));
+                txt_Lon.setText(String.valueOf(location.getLongitude()));
+            }
+
+        });
     }
 }
